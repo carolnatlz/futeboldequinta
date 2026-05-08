cca409164966c42fde4fca58914e7575d86fc8f3
carolnatlz <carolina.natalizi@gmail.com>
2026-05-08 19:29:59 -0300

    várias telas

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,12 +1,33 @@
 from flask import render_template
-from flask_login import login_required
+from app.routes import main
 
-from . import main
+SEQUENCIA_DOS_JOGOS = [
+    {"rodada": 1, "jogos": ["A x B", "C x D", "E x F"]},
+    {"rodada": 2, "jogos": ["A x C", "B x E", "D x F"]},
+    {"rodada": 3, "jogos": ["B x C", "F x A", "E x D"]},
+    {"rodada": 4, "jogos": ["F x B", "C x E", "D x A"]},
+]
 
 
 @main.route("/")
 @main.route("/home")
-@login_required
 def home():
     return render_template("components/home.html")
 
+
+@main.route("/agenda")
+def agenda():
+    return render_template("components/agenda.html")
+
+
+@main.route("/sequencia-dos-jogos")
+def sequencia_dos_jogos():
+    return render_template(
+        "components/sequencia_dos_jogos.html",
+        rodadas=SEQUENCIA_DOS_JOGOS,
+    )
+
+
+@main.route("/regras")
+def regras():
+    return render_template("components/regras.html")
